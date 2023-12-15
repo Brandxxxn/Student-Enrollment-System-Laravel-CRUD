a9458302336f5338ccb24dd27e76749e066a4d14
nicolaslaracaicedo <nalara2@espe.edu.ec>
2023-12-15 08:13:13 -0500

    add files of project

---
+++ database/migrations/2023_12_15_015553_create_docentes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Docentes', function (Blueprint $table) {
+            $table->integer('id_docente')->primary();
+            $table->string('nombre_docente', 255);
+            $table->string('apellido_docente', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('docentes');
+    }
+};
